2facc0d51fa8317589bcdbc0b14df77dca560e61
bleucanary <cowens@coroware.com>
2015-09-04 14:08:39 -0500

    Adding a test recipe

---
+++ ConeectionStreamer.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2015 cameron <cameron@Megatron-Virtual>
+#
+# Distributed under terms of the MIT license.
+
+"""
+This is a basic test recipe from picamear docs for a network stream
+"""
+
+import socket
+import time
+import picamera
+
+with picamera.PiCamera() as camera:
+    camera.resolution = (640, 480)
+    camera.framerate = 24
+
+    server_socket = socket.socket()
+    server_socket.bind(('192.168.1.130', 8000))
+    server_socket.listen(0)
+
+    conecction = server_socket.accept()[0].makefile('wb')
+    try:
+        camera.start_recording(connection, format='h264')
+        camera.wait_recording(60)
+        camera.stop_recording()
+    finally:
+        connection.close()
+        server_socket.close()
