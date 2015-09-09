39e1746cf0855c6a52d24dc27260fe09d2ecfe0d
bleucanary <cowens@coroware.com>
2015-09-09 17:30:37 -0500

    Updated License information

--- PythonBasics/Blinky.py
+++ PythonBasics/Blinky.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # vim:fenc=utf-8
 #
-# Copyright © 2015 cameron <cameron@Megatron-Virtual>
+# Copyright © 2015 CoroWare Robotics Solutions <>
 #
 # Distributed under terms of the MIT license.
 
