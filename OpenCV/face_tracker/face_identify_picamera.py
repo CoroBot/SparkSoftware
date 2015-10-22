c8d09a91ea4c0c438ece13ef98f0979478c88e91
bleucanary <cowens@coroware.com>
2015-10-22 18:13:35 -0500

    Changed Cascade flags

--- OpenCV/face_tracker/face_identify_picamera.py
+++ OpenCV/face_tracker/face_identify_picamera.py
@@ -32,7 +32,7 @@ for frame in camera.capture_continuous(rawCapture, format='bgr',
                                           scaleFactor=1.1,
                                           minNeighbors=5,
                                           minSize=(30, 30),
-                                          flags=cv2.cv.CV_HAAR_SCALE_IMAGE)
+                                          flags=cv2.CASCADE_SCALE_IMAGE)
     for (x, y, w, h) in faces:
         cv2.rectangle(image, (x, y), (x+w, y+h), (0, 0, 255), 2)
         center_point = ((x+(x+w))/2, (y+(y+h))/2)
