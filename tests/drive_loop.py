60b9a26ac5eba6003797e92e9e01fef4a2c92d0c
MetaEmpire <keenanempire@gmail.com>
2015-11-02 14:37:54 -0800

    first pass at drive_loop.py

--- tests/drive_loop.py
+++ tests/drive_loop.py
@@ -4,7 +4,17 @@ import sys
 from Spark_Control import HID_Comm, NET_ZMQ_Comm, Spark_Drive
 import time
 
+'''
+CoroBot spark test script - "drive_loop.py"
+
+This script lets the spark drive itself over a small area autonomously.
+
+By looking at this example code, you can learn how to control the motors
+on the spark kit, as well as insert software delays between actions.
+'''
+
 def main():
+	'''
 	addr = raw_input("Address of target (tcp://its.ip.add.ress:port) or hit enter for local USB:")
 	if addr == "-":
 		addr = "tcp://raspberrypi.local:4567"
@@ -16,19 +26,22 @@ def main():
 			print "Spark not found:",ex
 			sys.exit()
 	else:
-		comm = NET_ZMQ_Comm()
-		try:
-			comm.open(addr)
-		except:
-			print "Error connecting to Spark"
-			sys.exit()
+	'''
+	addr = "tcp://raspberrypi.local:4567"
+	comm = NET_ZMQ_Comm()
+	try:
+		comm.open(addr)
+	except:
+		print "Error connecting to Spark"
+		sys.exit()
 	
 	spark = Spark_Drive(comm)
 	drive_loop(spark)
 	
 def drive_loop(spark):
 	while(1):
-		print("Driving forward")
+		#drive forward
+		print("Driving forward for 3 seconds...")
 		spark.set_motor_speed(6, 12500)
 		spark.set_motor_speed(5, 12500)
 		#repeat command to try and get more consistent sets on motor speed.
@@ -37,26 +50,27 @@ def drive_loop(spark):
 		
 		time.sleep(3)
 	
-		#turn around #or for now, stop.
-		print("stopping for 2")
+		#stop
+		print("Stopping for 1 second...")
 		spark.set_motor_speed(6, 0)
 		spark.set_motor_speed(5, 0)
 		#repeat command
 		spark.set_motor_speed(6, 0)
 		spark.set_motor_speed(5, 0)	
-		time.sleep(2)
+		time.sleep(1)
 
-		#turn around
+		#turn around (skid steer)
+		print("Turning around for 2 seconds...")
 		spark.set_motor_direction(6, 1)
 		spark.set_motor_direction(6, 1)
-		#spark.set_motor_direction(5, 1)
-		#spark.set_motor_direction(5, 1)
 		spark.set_motor_speed(6, 61000)
 		spark.set_motor_speed(5, 61000)
 		spark.set_motor_speed(6, 61000)
 		spark.set_motor_speed(5, 61000)
 		time.sleep(2)
 	
+		#stop turning and set motors back to forward direction
+		print("Stopping for 1 second and then repeating...")
 		spark.set_motor_direction(5, 0)
 		spark.set_motor_direction(6, 0)
 		spark.set_motor_direction(5, 0)
@@ -65,7 +79,7 @@ def drive_loop(spark):
 		spark.set_motor_speed(6, 0)
 		spark.set_motor_speed(5, 0)
 		spark.set_motor_speed(6, 0)
-		time.sleep(2)
+		time.sleep(1)
 		
 	
 if __name__ == '__main__':
